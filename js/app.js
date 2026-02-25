3f6d8152e882ce3599248386215b5db5abc18555
Val <60014136+voffee@users.noreply.github.com>
2026-02-26 01:54:16 +0800

    canvas setup

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 
 // alert('Hello World');
+
+const canvas = document.querySelector(`#etch-a-sketch`);
+const ctx = canvas.getContext(`2d`);
+const shakeButton = document.querySelector(`.shake`);
+
+ctx.lineJoin = `round`;
+ctx.lineCap = `round`;
+ctx.lineWidth = 10;
+
+ctx.beginPath();
+ctx.moveTo(100, 200);
+ctx.lineTo(100, 200);
+ctx.stroke();
