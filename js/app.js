1367bd1389c542d26d57d1008df5546f3b26f54a
Val <60014136+voffee@users.noreply.github.com>
2026-02-26 08:11:30 +0800

    random number function created but need to refactor code.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 const canvas = document.querySelector(`#etch-a-sketch`);
 const ctx = canvas.getContext(`2d`);
 const shakeButton = document.querySelector(`.shake`);
+const {width, height} = canvas;
 
 ctx.lineJoin = `round`;
 ctx.lineCap = `round`;
@@ -13,3 +14,14 @@ ctx.beginPath();
 ctx.moveTo(100, 200);
 ctx.lineTo(100, 200);
 ctx.stroke();
+
+// Create Random Starting Position Functions
+
+console.log(width, height);
+
+function randomStart(maxWidth, maxHeight) {
+    widthPosition = Math.floor(Math.random() * maxWidth + 1);
+    heightPosition = Math.floor(Math.random() * maxHeight + 1);
+
+    return [widthPosition, heightPosition];
+}
