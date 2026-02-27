e54f84521db042049c9ffdb3bd5e08b439a9bc32
Val <60014136+voffee@users.noreply.github.com>
2026-02-28 07:59:35 +0800

    fixed rng function (working) + created keyhandler function.

--- js/app.js
+++ js/app.js
@@ -5,23 +5,30 @@ const canvas = document.querySelector(`#etch-a-sketch`);
 const ctx = canvas.getContext(`2d`);
 const shakeButton = document.querySelector(`.shake`);
 const {width, height} = canvas;
+const [startX, startY] =  randomStart(width, height);
+
+// Function Declarations
+function randomStart(width, height) {
+    const widthPosition = Math.floor(Math.random() * width + 1);
+    const heightPosition = Math.floor(Math.random() * height + 1);
+
+    return [widthPosition, heightPosition];
+}
+
+function handleKey(e) {
+    // e.preventDefault();
+    console.log(`Handling Key`);
+}
 
 ctx.lineJoin = `round`;
 ctx.lineCap = `round`;
 ctx.lineWidth = 10;
 
 ctx.beginPath();
-ctx.moveTo(100, 200);
-ctx.lineTo(100, 200);
+ctx.moveTo(startX, startY);
+ctx.lineTo(startX, startY);
 ctx.stroke();
 
-// Create Random Starting Position Functions
+window.addEventListener('keydown', handleKey);
 
 console.log(width, height);
-
-function randomStart(maxWidth, maxHeight) {
-    widthPosition = Math.floor(Math.random() * maxWidth + 1);
-    heightPosition = Math.floor(Math.random() * maxHeight + 1);
-
-    return [widthPosition, heightPosition];
-}
