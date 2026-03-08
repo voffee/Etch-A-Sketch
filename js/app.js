b8c0288d1f85497fbbdb130c81c1daf77813957e
Val <60014136+voffee@users.noreply.github.com>
2026-03-08 10:52:38 +0800

    draw function complete.

--- js/app.js
+++ js/app.js
@@ -5,12 +5,13 @@ const canvas = document.querySelector(`#etch-a-sketch`);
 const ctx = canvas.getContext(`2d`);
 const shakeButton = document.querySelector(`.shake`);
 const {width, height} = canvas;
-const [startX, startY] =  randomStart(width, height);
+const MOVE_AMOUNT = 5;
+let [startX, startY] =  randomStart(width, height);
 
 // Function Declarations
 function randomStart(width, height) {
-    const widthPosition = Math.floor(Math.random() * width + 1);
-    const heightPosition = Math.floor(Math.random() * height + 1);
+    let widthPosition = Math.floor(Math.random() * width + 1);
+    let heightPosition = Math.floor(Math.random() * height + 1);
 
     return [widthPosition, heightPosition];
 }
@@ -18,6 +19,35 @@ function randomStart(width, height) {
 function draw({ key }) {
     // Used object destructuring instead of receiving passed object and isolating property we want.
     console.log(key);
+
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    // startX = startX - MOVE_AMOUNT;
+    // startY = startY - MOVE_AMOUNT;
+    
+    switch (key) {
+        case `ArrowUp`:
+            startY = startY - MOVE_AMOUNT;
+            break;
+
+        case `ArrowDown`:
+            startY = startY + MOVE_AMOUNT;
+            break;
+
+        case `ArrowLeft`:
+            startX = startX - MOVE_AMOUNT;
+            break;
+
+        case `ArrowRight`:
+            startX = startX + MOVE_AMOUNT;
+            break;
+
+        default:
+            break;
+    }
+
+    ctx.lineTo(startX, startY);
+    ctx.stroke();
 }
 
 function handleKey(e) {
@@ -29,8 +59,10 @@ function handleKey(e) {
 
 ctx.lineJoin = `round`;
 ctx.lineCap = `round`;
-ctx.lineWidth = 10;
+ctx.lineWidth = 5;
 
+let hue = 0;
+ctx.strokeStyle = `hsl(100, 100%, 50%)`;
 ctx.beginPath();
 ctx.moveTo(startX, startY);
 ctx.lineTo(startX, startY);
